0ef30ed09d9802283f5d7d7313aebb580374daee
Sergey Pechenyuk <sergey.pechenyuk@gmail.com>
2018-08-01 07:23:01 +0300

    Update composer

--- src/ShortUrl.php
+++ src/ShortUrl.php
@@ -1,9 +1,13 @@
 <?php
 namespace Sergeypechenyuk\ShortUrl;
 
+use GuzzleHttp;
+
 class ShortUrl {
     public function create($url)
     {
-        return $url;
+        $client = new GuzzleHttp\Client();
+        $response = $client->get('https://mail.ru');
+        dd($result);
     }
 }

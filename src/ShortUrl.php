43f08a4e97216a1e4a0d5f093a5f9db645450b99
Sergey Pechenyuk <sergey.pechenyuk@gmail.com>
2018-08-01 08:51:53 +0300

    Update ShortUrl library

--- src/ShortUrl.php
+++ src/ShortUrl.php
@@ -1,18 +1,60 @@
 <?php
 namespace Sergeypechenyuk\ShortUrl;
 
-use GuzzleHttp;
+use Curl\Curl;
 
 class ShortUrl {
     public function create($url)
     {
         $curl = new Curl();
-//        $curl->setOpt(CURLOPT_PROXY, $proxy);
+        $curl->setHeader('X-SERVER-TOKEN', config("shorturl.server-token"));
+        $curl->setHeader('X-SERVER-SECRET', config("shorturl.server-secret"));
+
         $curl->post(config("shorturl.api-url")."links/create", [
             'long_url' => $url,
         ]);
-dd($curl->response);
-        $response = $client->get('https://mail.ru');
-        dd($result);
+
+        return json_decode($curl->response);
+    }
+
+    public function remove($url)
+    {
+        $curl = new Curl();
+        $curl->setHeader('X-SERVER-TOKEN', config("shorturl.server-token"));
+        $curl->setHeader('X-SERVER-SECRET', config("shorturl.server-secret"));
+
+        $curl->post(config("shorturl.api-url")."links/remove", [
+            'short_url' => $url,
+        ]);
+
+        return json_decode($curl->response);
+    }
+
+    public function stats($url, $details = 0)
+    {
+        $curl = new Curl();
+        $curl->setHeader('X-SERVER-TOKEN', config("shorturl.server-token"));
+        $curl->setHeader('X-SERVER-SECRET', config("shorturl.server-secret"));
+
+        $curl->post(config("shorturl.api-url")."links/stats", [
+            'short_url' => $url,
+            "details" => $details,
+        ]);
+
+        return json_decode($curl->response);
+    }
+
+    public function statsAll($urls, $details = 0)
+    {
+        $curl = new Curl();
+        $curl->setHeader('X-SERVER-TOKEN', config("shorturl.server-token"));
+        $curl->setHeader('X-SERVER-SECRET', config("shorturl.server-secret"));
+
+        $curl->post(config("shorturl.api-url")."links/stats/all", [
+            'short_urls' => $urls,
+            "details" => $details,
+        ]);
+
+        return json_decode($curl->response);
     }
 }

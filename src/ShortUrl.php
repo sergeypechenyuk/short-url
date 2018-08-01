36d827cf1c30a3bd09cc8061e9ba2c66bdf95ea9
Sergey Pechenyuk <sergey.pechenyuk@gmail.com>
2018-08-01 07:46:13 +0300

    Update ShortUrl library

--- src/ShortUrl.php
+++ src/ShortUrl.php
@@ -6,7 +6,12 @@ use GuzzleHttp;
 class ShortUrl {
     public function create($url)
     {
-        $client = new GuzzleHttp\Client();
+        $curl = new Curl();
+//        $curl->setOpt(CURLOPT_PROXY, $proxy);
+        $curl->post(config("shorturl.api-url")."links/create", [
+            'long_url' => $url,
+        ]);
+dd($curl->response);
         $response = $client->get('https://mail.ru');
         dd($result);
     }
